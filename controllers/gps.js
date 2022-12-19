5e71b4c978529a28e0963ae3275c677247827ae4
jb <jb.tournez@gmail.com>
2022-12-19 19:25:36 +0100

    e

--- controllers/gps.js
+++ controllers/gps.js
@@ -29,7 +29,7 @@ exports.getGPSByUserId = (req, res, next) => {
 exports.getGPSClose = (req, res, next) => {
     let latitude = req.params.id.split('+')[0];
     let longitude = req.params.id.split('@')[1];
-    GPS.find({ latitude:{ $gte: latitude - 0.05 && longitude - 0.05, $lte: latitude + 0.05 && longitude + 0.05} })
+    GPS.find({ latitude:{ $gte: latitude - 0.05 && longitude - 0.05} })
     .then(gps => res.status(200).json(gps))
     .catch(error => res.status(400).json({ error }));
 };
